c2df2bd70ec2b93b96bab9d1d78ff7ab49fbcf91
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-02-07 19:02:52 +0800

    Add upgrade.sh

---
+++ bin/upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Don't forget to make the file executable `chmod a+x update.sh`
+
+echo "Running \`brew upgrade\`"
+brew upgrade
+echo "Running \`mas upgrade\`"
+mas upgrade
+echo "Running \`softwareupdate -i -a\`"
+softwareupdate -i -a
