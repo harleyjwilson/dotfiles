4f7470ff8a61a1fb3250bdea7eda05abfbffa16c
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-02-07 19:02:33 +0800

    Update update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -2,6 +2,11 @@
 
 # Don't forget to make the file executable `chmod a+x update.sh`
 
-brew upgrade
-mas upgrade
-softwareupdate -i -a
+echo "Running \`brew update\`"
+brew update
+echo "Running \`brew outdated\`"
+brew outdated
+echo "Running \`mas outdated\`"
+mas outdated
+echo "Running \`softwareupdate -l\`"
+softwareupdate -l
