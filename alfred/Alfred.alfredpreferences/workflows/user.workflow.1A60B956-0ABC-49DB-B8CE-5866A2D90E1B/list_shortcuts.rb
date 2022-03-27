20216bc32a02af182b721366e046d9a106178cf2
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-03-27 21:04:04 +0800

    Remove alfred preferences

--- alfred/Alfred.alfredpreferences/workflows/user.workflow.1A60B956-0ABC-49DB-B8CE-5866A2D90E1B/list_shortcuts.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'shortcuts/executable'
-require_relative 'shortcuts/output'
-
-require_relative 'alfred/item'
-require_relative 'alfred/output'
-
-puts Shortcuts::Output.build
