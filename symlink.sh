d4759067619d96242fde9d1946e7449420dd78c6
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-06-26 15:56:47 +0800

    Update symlink.sh script

--- symlink.sh
+++ symlink.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-echo "Symbolic linking of dotfiles..."
+echo "Symlinking dotfiles..."
 
 # Symlink directories
 ln -sf ~/dotfiles/bin ~/bin
-# ln -sf ~/dotfiles/.config ~/.config
+echo "Linking ~/dotfiles/bin -> ~/bin"
 
 # Symlink files
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+echo "Linking ~/dotfiles/.gitconfig -> ~/.gitconfig"
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+echo "Linking ~/dotfiles/.tmux.conf -> ~/.tmux.conf"
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+echo "Linking ~/dotfiles/.vimrc -> ~/.vimrc"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+echo "Linking ~/dotfiles/.zshrc -> ~/.zshrc"
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
+echo "Linking ~/dotfiles/Brewfile -> ~/Brewfile"
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
+echo "Linking ~/dotfiles/init.vim -> ~/.config/nvim/init.vim"
+
+
+echo "Symlinking complete."
