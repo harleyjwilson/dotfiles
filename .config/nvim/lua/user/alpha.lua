69dbf4ac3ecdc72e068cc409bb0197b7d79f0640
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-10-28 07:18:25 +0800

    Update nvim alpha.lua

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -13,16 +13,21 @@ dashboard.section.header.val = {
   [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-  dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+  dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+  dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
   dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
   dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "NVIM v0.8.0"
+    local f = [[
+    NVIM v0.8.0
+    Build type: Release
+    LuaJIT 2.1.0-beta3
+    ]]
+  return f 
 end
 
 dashboard.section.footer.val = footer()
