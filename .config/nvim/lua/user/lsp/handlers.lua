df6399aab9ac346bc9ee645390aa4f77e67dc582
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-10-30 19:34:35 +0800

    Update lsp keymaps

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -54,26 +54,84 @@ end
 local function lsp_keymaps(bufnr)
 	local opts = { noremap = true, silent = true }
 	local keymap = vim.api.nvim_buf_set_keymap
- -- keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	-- keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	-- keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	-- keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    -- keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-    -- keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	-- keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	-- keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
-    keymap(bufnr, "n", "lR", "<cmd>lua vim.lsp.buf.references()<CR>", {desc="References", noremap=true, silent=true})
-    keymap(bufnr, "n", "ld", "<cmd>lua vim.lsp.buf.definition()<CR>",{desc="Definition", noremap=true, silent=true} )
-	keymap(bufnr, "n", "lf", "<cmd>lua vim.diagnostic.open_float()<CR>", {desc="Float Diagnostic", noremap=true, silent=true})
-    keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", { desc="Format", noremap=true, silent=true})
-    keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", {desc ="Info", noremap = true, silent = true})
-    keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", {desc="Installer Info", noremap = true, silent = true})
-    keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", {desc="Next Diagnostic", noremap = true, silent = true})
-    keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", {desc="Prev Diagnostic", noremap = true, silent = true})
-    keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", {desc="Rename", noremap = true, silent = true})
-    keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", {desc ="Quickfix", noremap = true, silent = true})
+	keymap(
+		bufnr,
+		"n",
+		"gr",
+		"<cmd>lua vim.lsp.buf.references()<CR>",
+		{ desc = "References", noremap = true, silent = true }
+	)
+	keymap(
+		bufnr,
+		"n",
+		"gd",
+		"<cmd>lua vim.lsp.buf.definition()<CR>",
+		{ desc = "Definition", noremap = true, silent = true }
+	)
+	keymap(
+		bufnr,
+		"n",
+		"gl",
+		"<cmd>lua vim.diagnostic.open_float()<CR>",
+		{ desc = "Diagnostic", noremap = true, silent = true }
+	)
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lf",
+		"<cmd>lua vim.lsp.buf.format{ async = true }<cr>",
+		{ desc = "Format", noremap = true, silent = true }
+	)
+	keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "Info", noremap = true, silent = true })
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lI",
+		"<cmd>LspInstallInfo<cr>",
+		{ desc = "Installer Info", noremap = true, silent = true }
+	)
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lj",
+		"<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>",
+		{ desc = "Next Diagnostic", noremap = true, silent = true }
+	)
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lk",
+		"<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>",
+		{ desc = "Prev Diagnostic", noremap = true, silent = true }
+	)
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lr",
+		"<cmd>lua vim.lsp.buf.rename()<cr>",
+		{ desc = "Rename", noremap = true, silent = true }
+	)
+	keymap(
+		bufnr,
+		"n",
+		"<leader>lq",
+		"<cmd>lua vim.diagnostic.setloclist()<CR>",
+		{ desc = "Quickfix", noremap = true, silent = true }
+	)
 end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
+		client.server_capabilities.documentFormattingProvider = false
+	end
+
+	if client.name == "jdtls" then
 		client.server_capabilities.documentFormattingProvider = false
 	end
 
