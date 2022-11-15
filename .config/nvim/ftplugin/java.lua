9b0baf1dc4a39bc61eef36faef09b5a5204792ab
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-11-15 09:04:02 +0800

    Update java.lua.save

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -205,11 +205,11 @@ jdtls.start_or_attach(config)
 
 require('jdtls').setup_dap()
 
-vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
-vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)"
-vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()"
+-- vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
+-- vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)"
+-- vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()"
 -- vim.cmd "command! -buffer JdtJol lua require('jdtls').jol()"
-vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
+-- vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
 -- vim.cmd "command! -buffer JdtJshell lua require('jdtls').jshell()"
 
 local status_ok, which_key = pcall(require, "whichkey")
@@ -260,4 +260,4 @@ which_key.register(mappings, opts)
 which_key.register(vmappings, vopts)
 
 -- debugging
--- git clone git@github.com:microsoft/java-debug.git
+-- git clone git@gitub.com:microsoft/java-debug.git
